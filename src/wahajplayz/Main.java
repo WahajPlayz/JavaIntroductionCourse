9934ebcfb23027872a6258ccdcaa869c4f299c24
WahajPlayz <161223768+WahajPlayz@users.noreply.github.com>
2025-11-01 20:03:53 +0000

    Added a Manual Line

--- src/wahajplayz/Main.java
+++ src/wahajplayz/Main.java
@@ -4,6 +4,10 @@ public class Main {
     public static final void main(String[] args){
         System.out.println("Hello World I am starting Up learning Minecraft Modding");
 
+
+        // This is the code that I wrote manually
+        System.out.println("Started Learning Java at 01/11/25 I hope I can acheve learning java and make a great minecraft mod or join a great minecraft modding team!!");
+
         // If you want to learn Minecraft Modding here is the link https://www.youtube.com/playlist?list=PLKGarocXCE1Egp6soRNlflWJWc44sau40
     }
 }
