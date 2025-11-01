9e062985df5d6f26ba97c36262f0c0662584d657
WahajPlayz <161223768+WahajPlayz@users.noreply.github.com>
2025-11-01 19:52:36 +0000

    Added a little message to the start

--- src/wahajplayz/Main.java
+++ src/wahajplayz/Main.java
@@ -1,5 +1,9 @@
+package wahajplayz;
+
 public class Main {
     public static final void main(String[] args){
-        System.out.println("Hello World");
+        System.out.println("Hello World I am starting Up learning Minecraft Modding");
+
+        // If you want to learn Minecraft Modding here is the link https://www.youtube.com/playlist?list=PLKGarocXCE1Egp6soRNlflWJWc44sau40
     }
 }
