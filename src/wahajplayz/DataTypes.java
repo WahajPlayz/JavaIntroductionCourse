250b3681103d8d4731096e513b91efec87270643
WahajPlayz <161223768+WahajPlayz@users.noreply.github.com>
2025-11-02 16:31:03 +0000

    Learned Data Type

---
+++ src/wahajplayz/DataTypes.java
@@ -0,0 +1,47 @@
+package wahajplayz;
+
+public class DataTypes {
+    /* DATA TYPES & VARIABLES */
+
+    // Hello I am Using a Multiline Comment!
+    /*
+     * And I am a multiline Comment!
+     * I can come in quite handy for explaining stuff!
+     */
+
+    /* Variables */
+
+    // datatype name_of_variable; // Declaration
+    // OR
+    // datatype name_of_variable = value_of_variable; // Initialization (Declaration + Assignment of Value)
+
+    /* DATA TYPES */
+
+
+    // Integers (Store whole numbers (including negative ones))
+    // Go from -2147483648 to 2147483647
+    int points = 100;
+    int x = -69;
+
+
+    // Floating Point Numbers (float & double)
+    float pi = 3.14159265f;
+    double e = 2.718281828;
+
+    // For real numbers, however! Precision can be an issue!
+    // Scientific notation: 314159265 * 10^-8
+
+
+    // Booleans (True or False)
+    boolean likedVideo = true;
+    boolean subscribed = true;
+
+
+    // Chars & Strings
+    char grade = 'B';
+    String name = "WahajPlayz";
+
+    //Chars is Singular Charecter with '' not ""
+    // String is a Bunch of Chars but you use "" instead of ''
+
+}
